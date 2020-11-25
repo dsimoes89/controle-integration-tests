48a16fe3bf50a50318a24e4a22feb9b042b22f15
Daniel Souza <dsimoes.souza@gmail.com>
2020-11-25 11:31:08 -0300

    First commit

---
+++ spec/support/database.rb
@@ -0,0 +1,12 @@
+require "pg"
+
+class Database
+  def initialize
+    conn = { host: "localhost", dbname: "database_dev", user: "postgres", password: "docker" }
+    @connection = PG.connect(conn)
+  end
+
+  def db_query(query)
+    @connection.exec(query)
+  end
+end
